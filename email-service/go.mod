5cb9bdb35728e3bb37d1de5cebf162922a6f90b9
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-10-30 19:07:47 +0700

    feat: add email-service

---
+++ email-service/go.mod
@@ -0,0 +1,5 @@
+module github.com/yehezkiel1086/go-gin-rabbitmq-email-notif/email-service
+
+go 1.24.4
+
+require github.com/rabbitmq/amqp091-go v1.10.0
---
+++ email-service/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/yehezkiel1086/go-gin-rabbitmq-email-notif/email-service/messaging/rabbitmq"
+)
+
+type UserResponse struct {
+	Email string `json:"email"`
+	Name  string `json:"name"`
+}
+
+func main() {
+	// init rabbitmq
+	mq, err := rabbitmq.InitRabbitMQ()
+	if err != nil {
+		log.Fatalf("Failed to initialize RabbitMQ: %v", err)
+	}
+
+	// declare channel
+	ch, err := mq.DeclareChannel()
+	if err != nil {
+		log.Fatalf("Failed to declare channel: %v", err)
+	}
+
+	// declare queue
+	q, err := mq.DeclareQueue(ch, "user_registered")
+	if err != nil {
+		log.Fatalf("Failed to declare queue: %v", err)
+	}
+
+	// consume messages
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,  // auto-ack
+		false, // exclusive
+		false, // no-local
+		false, // no-wait
+		nil,   // args
+	)
+	if err != nil {
+		log.Fatalf("Failed to register consumer: %v", err)
+	}
+
+	log.Println("📨 Waiting for messages...")
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			var user UserResponse
+			err := json.Unmarshal(d.Body, &user)
+			if err != nil {
+				log.Printf("Invalid message format: %v", err)
+				continue
+			}
+			log.Printf("📧 Sending welcome email to %s <%s>", user.Name, user.Email)
+		}
+	}()
+
+	<-forever
+}
